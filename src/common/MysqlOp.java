db5d48d6ccce697cc32385cb17ca37dc6ec0ac29
lirunquan <1617277400@qq.com>
2018-06-20 23:31:00 +0800

    06/20 23:30

--- src/common/MysqlOp.java
+++ src/common/MysqlOp.java
@@ -1,10 +1,13 @@
 package common;
+import java.awt.List;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MysqlOp {
-//	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
 	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-//	static final String DB_URL = "jdbc:mysql://192.168.1.166:3306/?useSSL=false";
-	static final String DB_URL = "jdbc:mysql://localhost:3306/?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=UTC";
+	static final String DB_URL = "jdbc:mysql://172.16.116.32:3306/?useUnicode=true&characterEncoding=utf-8&useSSL=false&serverTimezone=UTC";
 	static final String USER = "root";
 	static final String PASSWORD = "201625010412";
 	
@@ -114,5 +117,36 @@ public class MysqlOp {
 			}
 		}
 		return data.equals(data_);
+	}
+	public ArrayList getInfo(String databaseName, String operation, String data) throws SQLException, ClassNotFoundException {
+		
+		ResultSet rSet = null;
+		Connection connection = null;
+		Statement statement = null;
+		Class.forName(JDBC_DRIVER);
+		connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+		String sql = "USE "+databaseName;
+		statement = connection.createStatement();
+		statement.executeUpdate(sql);
+			
+		statement = connection.createStatement();
+		rSet = statement.executeQuery(operation);
+		connection.close();
+		statement.close();
+		return  convertList(rSet);
+	}
+	private static ArrayList convertList(ResultSet rSet) throws SQLException{
+		ArrayList list = new ArrayList();
+		ResultSetMetaData metaData = rSet.getMetaData();
+		int column = metaData.getColumnCount();
+		while(rSet.next()) {
+			Map rowData = new HashMap();
+			for(int i=1; i<=column; i++) {
+				rowData.put(metaData.getColumnName(i), rSet.getObject(i));
+			}
+			list.add(rowData);
+		}
+		return list;
+		
 	}
 }
